56fc82db0c3faaf82748d2762376e66d0a0d519e
SLONGO-DANI <127991498+SLONGO-DANI@users.noreply.github.com>
2024-09-04 16:47:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+export const perguntas = [
+    {
+        enunciado: "como a sociedade pode prejudicar o ser humano?",
+        alternativas: [
+            {
+                texto: "O isolamento social ",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "dependência tecnológica",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "quais são os pontos negativos da sociedade?",
+        alternativas: [
+            {
+                texto: "viés e discriminação!",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "falta de interação humana!",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "quais os pontos positivos?",
+        alternativas: [
+            {
+                texto: "cidades inteligentes.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Notes.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+     
+];
